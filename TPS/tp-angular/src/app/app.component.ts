19a09e06e27941490290d38aa23bd2d8ab679462
AzureAD\EricSULTAN <eric@fifox.fr>
2024-05-28 10:42:30 +0200

    tp-angular-v2

--- TPS/tp-angular/src/app/app.component.ts
+++ TPS/tp-angular/src/app/app.component.ts
@@ -10,7 +10,19 @@ export class AppComponent {
   prenom: string = 'Eric';
   couleur: string = "#0000FF";
 
+  recherche: string = "";
+
   todo: Todo = new Todo();
+
+  todos: Array<Todo> = new Array<Todo>();
+
+  todoForm: Todo = new Todo();
+
+  constructor() {
+    this.todos.push(new Todo(2, "Etendre le linge", false, 1));
+    this.todos.push(new Todo(4, "Passer la tondeuse", false, 3));
+    this.todos.push(new Todo(5, "Régler la chaudière", true, 3));
+  }
 
   resetPrenom() {
     this.prenom  = "";
@@ -20,4 +32,19 @@ export class AppComponent {
     this.prenom = evt.toUpperCase();
   }
 
+  ajouterTodo() {
+    if(this.todoForm.id && this.todoForm.title) {
+      this.todos.push(this.todoForm);
+      this.todoForm = new Todo();
+    }
+  }
+
+
+  search(): Todo[] {
+    if(this.recherche) {
+      return this.todos.filter(todo => todo.title?.indexOf(this.recherche) != -1);
+    } 
+
+    return this.todos;
+  }
 }
